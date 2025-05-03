0e9d06a13743fbdbbe453ffa63d6ec34d0c3726b
thuatus <alvaro.junior@outlook.com>
2025-05-03 16:57:00 -0300

    implements issue server certificate

--- V-0.0.1/ca/ca.go
+++ V-0.0.1/ca/ca.go
@@ -3,9 +3,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"text/template"
 )
 
@@ -90,20 +92,20 @@ func createConfigFiles(caPath string, country string, state string, local string
 	}
 
 	if _, err := serialFile.WriteString("1000"); err != nil {
-		log.Fatalf("Error writing serial file: %v", err)
+		log.Fatalf("Error writing serialCA file: %v", err)
 		return "", err
 	}
 	defer serialFile.Close()
 
-	//create cerl files
+	//create crl files
 	crlFile, err := os.Create(caPath + "/crlnumber")
 	if err != nil {
-		log.Fatalf("Error creating crl file: %v", err)
+		log.Fatalf("Error creating crlCA file: %v", err)
 		return "", err
 	}
 
 	if _, err := crlFile.WriteString("100"); err != nil {
-		log.Fatalf("Error writing serial file: %v", err)
+		log.Fatalf("Error writing crlCA file: %v", err)
 		return "", err
 	}
 	defer crlFile.Close()
@@ -115,6 +117,41 @@ func createConfigFiles(caPath string, country string, state string, local string
 		return "", err
 	}
 	defer indexFile.Close()
+
+	//create index Intermediate CA file
+
+	serialIntFile, err := os.Create(caPath + "/intermediateCA/serial")
+	if err != nil {
+		log.Fatalf("Error creating serial file: %v", err)
+		return "", err
+	}
+
+	if _, err := serialIntFile.WriteString("1000"); err != nil {
+		log.Fatalf("Error writing serialCA file: %v", err)
+		return "", err
+	}
+	defer serialIntFile.Close()
+
+	//create crl files
+	crlIntCAFile, err := os.Create(caPath + "/intermediateCA/crlnumber")
+	if err != nil {
+		log.Fatalf("Error creating crlCA file: %v", err)
+		return "", err
+	}
+
+	if _, err := crlIntCAFile.WriteString("100"); err != nil {
+		log.Fatalf("Error writing crlCA file: %v", err)
+		return "", err
+	}
+	defer crlIntCAFile.Close()
+
+	//create index file
+	indexintCAFile, err := os.Create(caPath + "/intermediateCA/index.txt")
+	if err != nil {
+		log.Fatalf("Error creating index file: %v", err)
+		return "", err
+	}
+	defer indexintCAFile.Close()
 
 	//create config file
 	// Parse the templateCA file
@@ -165,7 +202,36 @@ func createConfigFiles(caPath string, country string, state string, local string
 		return "", err
 	}
 
+	defFilePath := "/home/alvaro/srv/CA/definitions.txt"
+	defFile, err := os.Create(defFilePath)
+	if err != nil {
+		log.Fatalf("Error creating definition file: %v", err)
+		return "", err
+	}
+
+	if _, err := defFile.WriteString(caPath + "|" + caPath + "/intermediateCA"); err != nil {
+		log.Fatalf("Error writing definition folder file: %v", err)
+		return "", err
+	}
+	defer defFile.Close()
 	return fileCAConfig.Name() + "\n" + fileIntConfig.Name(), nil
+}
+
+// get the CA folder info
+func getCAInfo() (string, string, error) {
+
+	// Lê o conteúdo do arquivo
+	caInfo, err := os.ReadFile("/home/alvaro/srv/CA/definitions.txt")
+	if err != nil {
+		fmt.Printf("can't write file %v\n", err)
+		return "", "", err
+	}
+
+	str := string(caInfo)
+
+	paths := strings.Split(str, "|")
+
+	return paths[0], paths[1], nil
 }
 
 // Creates CA certificate, private key and public key
@@ -213,7 +279,7 @@ func createCACertificate(caPathName string) (string, error) {
 
 	// create intermediate certificate based on intermediateCA.cnf
 	cmd = exec.Command("openssl", "ca", "-config", caConfigFIle, "-extensions", "v3_intermediate_ca", "-notext", "-in", intermediateCARequest, "-md", "sha256", "-out", intermediateCACertificate, "-days", "1825", "-batch")
-	fmt.Printf("--->%v", cmd)
+
 	intermediateCACertinfo, err := cmd.Output()
 	if err != nil {
 		log.Fatalf("Error creating intermediate CA certificate %v: %d", intermediateCACertinfo, err)
@@ -223,15 +289,68 @@ func createCACertificate(caPathName string) (string, error) {
 	return string(caKeyinfo) + "|" + string(caCertinfo), nil
 }
 
+//Issue a server certificate
+
+func issueServerCertificate(ServerName string, csr io.Reader) (string, error) {
+
+	fmt.Printf("getting CA information ...\n")
+	_, fintCAPath, err := getCAInfo()
+	if err != nil {
+		log.Fatalf("Error get CA definitions: %v", err)
+	}
+
+	serverCrt := fintCAPath + "/certs/" + ServerName + ".crt"
+	ServerReq := fintCAPath + "/csr/" + ServerName + ".csr"
+	csrInfo, err := io.ReadAll(csr)
+	if err != nil {
+		fmt.Println("Can't read csr file:", err)
+		return "", err
+	}
+	// Write the contents to a file
+	csrFile, err := os.Create(ServerReq)
+	if err != nil {
+		fmt.Println("Error creating CSR file:", err)
+		return "", err
+	}
+	defer csrFile.Close()
+	_, err = csrFile.Write(csrInfo)
+	if err != nil {
+		fmt.Println("Error writing CSR file:", err)
+		return "", err
+	}
+
+	// create server certificate based on intermediateCA.cnf
+
+	cmd := exec.Command("openssl", "ca", "-config", fintCAPath+"/intermediateCA.cnf", "-extensions", "server_cert", "-notext", "-md", "sha256", "-in", ServerReq, "-out", serverCrt, "-days", "365", "-batch")
+
+	serverCertinfo, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Error creating server certificate: %v, %v", serverCertinfo, err)
+		return "", err
+	}
+
+	// show the server certificate
+
+	cmd = exec.Command("openssl", "x509", "-in", serverCrt, "-text", "-noout")
+
+	serverCertinfo, err = cmd.Output()
+	if err != nil {
+		log.Fatalf("Error showing server certificate %v: %d", serverCertinfo, err)
+		return "", err
+	}
+	return string(serverCertinfo), nil
+}
+
 // main to validade
 func main() {
-	caName := "CA-10"
+	caName := "CA-01"
+
 	fmt.Printf("Creating CA ...\n")
 	caPathName, intermediateCaPathName, err := createPKIFolders(caName)
 	if err != nil {
 		log.Fatalf("Error creating CA Folders: %v", err)
 	}
-	log.Printf("Folder structure created!\nCA path: %s \n IntermediateCAPAth: %s", caPathName, intermediateCaPathName)
+	log.Printf("Folder structure created!\nCA path: %s \n IntermediateCAPAth: %s\n", caPathName, intermediateCaPathName)
 
 	fmt.Printf("Creating CA config files ...\n")
 	country := "BR"
@@ -252,5 +371,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error creating CA certificate: %v", err)
 	}
-	log.Printf("CA certificate created! %s", cacertificate)
+	log.Printf("CA certificate created! %s\n", cacertificate)
+
+	fmt.Printf("Creating Server certificate ...\n")
+	reqFile, err := os.Open("/home/alvaro/app01.csr")
+	if err != nil {
+		log.Fatalf("Error opening CSR file: %v", err)
+	}
+	defer reqFile.Close()
+	serverCert, err := issueServerCertificate("app01", reqFile)
+	if err != nil {
+		log.Fatalf("Error creating server certificate: %v", err)
+	}
+	log.Printf("Server certificate created! %s\n", serverCert)
+
 }
